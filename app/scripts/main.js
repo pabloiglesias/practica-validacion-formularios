62599fdaf714a6d728d4b65a656722960c895c53
pablo <exfaex2@gmail.com>
2015-02-23 16:01:54 +0100

    lunes 23

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,38 @@
-$("#Myform").validate();
+$(document).ready(function(){
+	$("#form_973471").validate({
+	event:blur,
+    rules: {
+      nombre: {
+        required: true,
+        lettersonly: true
+      },
+      apellidos: {
+        required: true,
+        lettersonly: true
+      },
+      telefono: {
+        required: true,
+        digits: true,
+        minlength: 9,
+	maxlength: 9
+      },
+      email: {
+        required: true,
+	email: true        
+      },
+      email2: {
+        required: true       
+      }, 
+   
+    },
+    messages: {
+ 
+      email: {
+        remote: 'email erroneo'
+              },
+     
+    }
+   
+    });
+});
+
