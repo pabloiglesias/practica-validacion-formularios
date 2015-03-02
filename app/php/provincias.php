b56e4fba5501da2108ce178ff4d7f5793151e135
pablo <exfaex2@gmail.com>
2015-03-03 00:58:19 +0100

    avanzando

--- app/php/provincias.php
+++ app/php/provincias.php
@@ -4,16 +4,18 @@ $cp = $_GET["cp"];
 header('content-type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *"); // permite usar CORS
 //Cadena de conexión seleccionando bd:
-@$userv = new mysqli("localhost", "pabloiglesias_bd", "provincias", "pabloiglesias_pro");
-$errorbd = $userv->connect_errno;
+$db = new mysqli("localhost", "pabloiglesias_bd", "provincias", "pabloiglesias_pro");
+$errorbd = $db->connect_errno;
 // si la conexión con la base de datos NO da error
 if ($errorbd == null) {
 //inicializamos el cliente en utf-8:
-	$userv->set_charset("utf8");
+	$db->set_charset("utf8");
 //creamos la consulta sobre la tabla provincias y localidades
-	$sql = "SELECT  poblacion FROM poblaciones where cod_postal=" . $cp+";";
-	while ($row = $result->fetch_assoc()) {
-		$data = $row['poblacion'];
+	$sql = "SELECT  poblacion FROM poblaciones where cod_postal=" . $cp . ";";
+	if (!$resultado = $db->query($sql)) {
+		die('Ocurrio un error ejecutando el query [' . $db->error . ']');}
+	while ($resul = $resultado->fetch_assoc()) {
+		$data = $resul['poblacion'];
 	}
 	$json = json_encode($data);
 	echo isset($_GET['callback'])
@@ -24,6 +26,6 @@ if ($errorbd == null) {
 	// si la conexión da error
 	print "Imposible conectar con la bbdd de provincias";
 }
-$userv->close();
+$db->close();
 
-unset($userv);
+unset($db);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,44 +26,52 @@
 	cif: {
 	required:true,
 	},
-nombreempresa: {required},
-direccion:{required:true,},
-cp: {
-required: true,
-minlength: 4,
-maxlength: 5,
-digits: true,
-remote:"provincias.php",
-},
-localidad: {
+	nombreempresa: {
+	required:true},
+	direccion:{
 	required:true,
-},
-provincia: "required",
-pais: "required",
-iban: {
-required: true,
-iban: true
-},
-usuario:{
-required: true,
-minlength: 4
-},
-contraseña: "required"
-},
-messages:{
+	},
+	cp: {
+	required: true,
+	minlength: 4,
+	maxlength: 5,
+	digits: true,
+	remote:'provincias.php'
+	},
+	localidad: {
+	required:true,
+	},
+	provincia:{ 
+	required:true
+	},
+	pais:{
+	required:true
+	},
+	iban:{
+	required: true	
+	},
+	usuario:{
+	required: true,
+	minlength: 4
+	},
+	contraseña:{
+	required:true
+	},
+	messages:{
 	nombre:{
-		required:"Campo obligatorio"
+		required:'Campo obligatorio'
 	},
 	apellidos:{
-		required:"Campo requerido"
+		required:'Campo requerido'
 	}
-},
+	},
 submitHanler:function(){
-	alert("");
+	alert('Esta en proceso de alta y su cuota sera '+$('#sel2').val()+' '+ $);
 }
 }
-};	
-$("#particular").change(function(){
+});
+ /*
+$('#particular').change(function(){
 if($('#particular').is(":checked")){
 $("#nombreemp").html("Nombre");
 $("#cifnif").html("NIF");
@@ -71,47 +79,49 @@ var completo = $("#nombre").val() + " " + $("#apellidos").val();
 $("#nombreempresa").val(completo);
 $("#nombreempresa").prop("readonly" , true);
 }
-});
-$("#empresa").change(function(){
-if($("#empresa").is(":checked")){
-$("#nombreemp").html("Empresa");
-$("#cifnif").html("CIF");
-$("#nombreempresa").val("");
-$("#nombreempresa").prop("readonly" , false);
+});*/
+$('#empresa').change(function(){
+if($('#empresa').is(':checked')){
+$('#nombreemp').html('Empresa');
+$('#cifnif').html('CIF');
+$('#nombreempresa').val('');
+$('#nombreempresa').prop('readonly', false);
 }
-})
-//relleno el CP con ceros si no tiene 5 dígitos y busco municipio y provincia en la base de datos
-$("#cp").focusout(function(){
-var dig= $("#cp").val();
-if(dig.lenght()==4){
-$("#cp").val("0" + dig);
-}
-var cp= $("#cp").val();
+});/*
+//relleno el CP con ceros si no tiene 5 dígitos busco provincia en la base de datos
+$('#cp').focusout(function(){
+var dig= $('#cp').val();
+if(dig.lenght()===4){
+$('#cp').val('0' + dig);
+}*/
+var cp=null;
+ cp= $('#cp').val();
 var promise = $.ajax({
-type: 'POST',
-"url" : "../php/provincias.php",
-"dataType": "json"
+type: 'GET',
+'url': 'php/provincias.php',
+'dataType': 'json'
 });
 promise.done(function(data){
-
-$('#provincia').attr('val',data['poblacion']);
+$('#provincia').attr('value',data).html();
 });
 promise.fail(function(){
-console.log("Error al importar municipio y provincia");
+console.log('Error al importar municipio y provincia');
 });
+$('#email').focusout(function(){
+
+	var usu=$('email').val();
+	$('#usuario').val(usu);
+	$('#usuario').prop('readonly' , true);
+
 });
+/*
 //cuando se completan los apellidos se rellena Nombre en los datos de facturación
 $("#apellidos").focusout(function(){
 var completo = $('#nombre').val() +' '+ $('#apellidos').val();
 $('#nombreempresa').val(completo);
 $('#nombreempresa').prop('readonly',true);
 });
-//se rellena el nombre de usuario con el mail
-$("#email").focusout(function(){
-var mail = $("#email").val();
-$('#usuario').val(mail);
-$('#usuario').prop('readonly' , true);
-});
+
 //establecer la password compleja
 $("#password").focusin(function(){
 $("#password").complexify(options, callback(valid, complexity)){
@@ -123,3 +133,4 @@ $('#password').complexify({}, function (valid, complexity) {
 $('#complex').attr('value',complexity);
 });
 });
+*/
