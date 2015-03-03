cfb3ec29b6fd97da05c3bdf70920e3ee2a80b62e
pablo <exfaex2@gmail.com>
2015-03-03 17:03:57 +0100

    casi terminado

--- app/php/login.php
+++ app/php/login.php
@@ -1,21 +1,22 @@
 <?php
-$email = $_GET["email"];
-// Cargar provincias
+$email = $_GET['email'];
+// Aceptamos peticiones cors
 header('content-type: application/json; charset=utf-8');
-header("Access-Control-Allow-Origin: *"); // permite usar CORS
-//Cadena de conexión seleccionando bd:
+header("Access-Control-Allow-Origin: *");
+//Cadena de conexión
 $db = new mysqli("localhost", "pabloiglesias_bd", "provincias", "pabloiglesias_pro");
 $errorbd = $db->connect_errno;
-// si la conexión con la base de datos NO da error
 if ($errorbd == null) {
 //inicializamos el cliente en utf-8:
 	$db->set_charset("utf8");
-//creamos la consulta sobre la tabla provincias y localidades
-	$sql = "SELECT  email FROM usuarios where email=" . $email . ";";
+//creamos la consulta sobre la usuarios
+	$sql = "SELECT email FROM usuarios;";
 	if (!$resultado = $db->query($sql)) {
 		die('Ocurrio un error ejecutando el query [' . $db->error . ']');}
 	while ($resul = $resultado->fetch_assoc()) {
-		$data = $resul['email'];
+		if ($email === $resul['email']) {
+			$data = $resul['email'];
+		}
 	}
 	$json = json_encode($data);
 	echo isset($_GET['callback'])
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,27 @@
  'use strict';
 $(document).ready(function(){
+	$.extend($.validator.messages, {
+	required: "Este campo es obligatorio.",
+	remote: "Por favor, rellena este campo.",
+	email: "Por favor, escribe una dirección de correo válida.",
+	url: "Por favor, escribe una URL válida.",
+	date: "Por favor, escribe una fecha válida.",
+	dateISO: "Por favor, escribe una fecha (ISO) válida.",
+	number: "Por favor, escribe un número válido.",
+	digits: "Por favor, escribe sólo dígitos.",
+	creditcard: "Por favor, escribe un número de tarjeta válido.",
+	equalTo: "Por favor, escribe el mismo valor de nuevo.",
+	extension: "Por favor, escribe un valor con una extensión aceptada.",
+	maxlength: $.validator.format("Por favor, no escribas más de {0} caracteres."),
+	minlength: $.validator.format("Por favor, no escribas menos de {0} caracteres."),
+	rangelength: $.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
+	range: $.validator.format("Por favor, escribe un valor entre {0} y {1}."),
+	max: $.validator.format("Por favor, escribe un valor menor o igual a {0}."),
+	min: $.validator.format("Por favor, escribe un valor mayor o igual a {0}."),
+	nifES: "Por favor, escribe un NIF válido.",
+	nieES: "Por favor, escribe un NIE válido.",
+	cifES: "Por favor, escribe un CIF válido."
+});
   $('#formulario').validate({
 	rules:{
 	nombre:{
@@ -18,7 +40,6 @@ $(document).ready(function(){
 	},
 	email: {
 	required: true,
-	email: true,
 	remote:'login.php'
 	},
 	email2:{
@@ -50,7 +71,10 @@ $(document).ready(function(){
 	required:true
 	},
 	iban:{
-	required: true	
+	required: true,
+	iban: true,
+	minlength: 24,
+	maxlength: 24
 	},
 	usuario:{
 	required: true,
@@ -69,6 +93,9 @@ $(document).ready(function(){
 	},
 	apellidos:{
 		required:'Campo requerido'
+	},
+	iban: {
+		iban:'Introduzca un IBAN correcto'
 	}
 	},
 submitHanler:function(){
@@ -94,7 +121,6 @@ $('#nombreempresa').val('');
 $('#nombreempresa').prop('readonly', false);
 }
 });
-//Examino cp si 4 numero añado cero por la izquierda4
 $('#cp').focusout(function(){
 var dig= $('#cp').val();
 if(dig.lenght()===4){
@@ -123,42 +149,33 @@ type: 'GET',
 'dataType': 'json'
 });
 promise.done(function(data){
-if(email===data){
+if(data===null){
 var usu=$('email').val();
-	$('#usuario').attr('value',usu).html();
+	$('#usuario').val(usu);
 	$('#usuario').prop('readonly' , true);
+
+}
+else
+{
+	alert('Error: Usuario ya existe');
 }
 });
 promise.fail(function(){
-alert('Error: El Usuario ya existe');
-	var usu=$('email').val();
-	$('#usuario').attr('value',usu).html();
-	$('#usuario').prop('readonly' , true);
-
+	alert('Error: al importar el correo');
 });
+});
+//complexyty
 $('#password').focusin(function () {
 $('#password').complexify({}, function (valid, complexity) {
 $('#complex').attr('value',complexity);
+
 });
 });
-/*
+
 //cuando se completan los apellidos se rellena Nombre en los datos de facturación
-$("#apellidos").focusout(function(){
+$('#apellidos').focusout(function(){
 var completo = $('#nombre').val() +' '+ $('#apellidos').val();
 $('#nombreempresa').val(completo);
 $('#nombreempresa').prop('readonly',true);
 });
-
-//establecer la password compleja
-$("#password").focusin(function(){
-$("#password").complexify(options, callback(valid, complexity)){
-	alert('Password complexity: '+ complexity);
-}
-});
-$('#password').focusin(function () {
-$('#password').complexify({}, function (valid, complexity) {
-$('#complex').attr('value',complexity);
-});
-});
-*/
 });
